62ef5f20138808103dc824b6777b8173af1f038b
hatskier <suvorovalex1997@gmail.com>
2021-07-20 13:35:46 +0200

    Quick fix in config generation tool

--- tools/config/generate-manifest-with-all-supported-tokens.js
+++ tools/config/generate-manifest-with-all-supported-tokens.js
@@ -4,7 +4,7 @@ const _ = require("lodash");
 const manifestsToExclude = [
   "5-top-tokens.json",
   "all-supported-tokens.json",
-  "yahoo-finance-rapid.json",
+  "api-dojo-rapid.json",
   "rapid.json",
   "stocks.json"
 ];
