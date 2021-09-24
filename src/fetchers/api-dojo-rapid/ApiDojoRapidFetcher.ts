6053b14b4efce547c5f08a652dac458f1020dcc3
hatskier <suvorovalex1997@gmail.com>
2021-09-24 03:37:03 +0200

    connected api-dojo-rapid provider to the new yh-finance api

--- src/fetchers/api-dojo-rapid/ApiDojoRapidFetcher.ts
+++ src/fetchers/api-dojo-rapid/ApiDojoRapidFetcher.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BaseFetcher } from "../BaseFetcher";
 import { FetcherOpts, PricesObj } from "../../types";
 
-const RAPID_API_HOST = "apidojo-yahoo-finance-v1.p.rapidapi.com";
+const RAPID_API_HOST = "yh-finance.p.rapidapi.com";
 const URL = `https://${RAPID_API_HOST}/market/v2/get-quotes`;
 const DEFAULT_REGION = "US";
 
