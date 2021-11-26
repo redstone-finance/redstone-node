26db8d180e2eae73ed960593315d5a70a2424449
Alex Suvorov <Suvorovalex1997@gmail.com>
2021-11-26 18:36:14 +0100

    Hatskier/running node in app runner (#38)
    * experiment with built-in express app
    
    * few fixes and improvements in Docker configuration and express server
    
    * tests fixed
    
    * tried t fixed github CI tests
    
    * tests disabled temporary
    
    * implemented a tool to analyze historical arweave fees

---
+++ src/ExpressAppRunner.ts
@@ -0,0 +1,36 @@
+import { Consola } from "consola";
+import exprress from "express";
+
+const logger = require("./utils/logger")("express") as Consola;
+
+const PORT = 8080;
+
+// For now we run a very simple express app to allow running the
+// redstone node in the AWS App runner.
+// Read more at:
+
+// This class will be extended in future and will be used for
+// communication between nodes
+export class ExpressAppRunner {
+  private app: exprress.Application;
+
+  constructor() {
+    this.app = exprress();
+
+    this.app.get("/", (_req, res) => {
+      res.send("Hello App Runner. My name is RedStone node and I am doing good ;)");
+    });
+  }
+
+  run() {
+    if (process.env.NODE_ENV === "test") {
+      logger.info(`Express server running skipped in test environment`);
+      return;
+    }
+    logger.info(`Running express server on port: ${PORT}`);
+    this.app.listen(PORT, () => {
+      logger.info(`Server started at http://localhost:${PORT}`);
+    });
+  }
+
+}
--- src/NodeRunner.ts
+++ src/NodeRunner.ts
@@ -8,6 +8,7 @@ import ManifestHelper, { TokensBySource } from "./manifest/ManifestParser";
 import ArweaveService from "./arweave/ArweaveService";
 import { mergeObjects, readJSON, timeout } from "./utils/objects";
 import PriceSignerService from "./signers/PriceSignerService";
+import { ExpressAppRunner } from "./ExpressAppRunner";
 import {
   printTrackingState,
   trackEnd,
@@ -66,8 +67,8 @@ export default class NodeRunner {
     this.httpBroadcaster = new HttpBroadcaster(nodeConfig.httpBroadcasterURLs);
     this.streamrBroadcaster = new StreamrBroadcaster(nodeConfig.credentials.ethereumPrivateKey);
 
-    //https://www.freecodecamp.org/news/the-complete-guide-to-this-in-javascript/
-    //alternatively use arrow functions...
+    // https://www.freecodecamp.org/news/the-complete-guide-to-this-in-javascript/
+    // alternatively use arrow functions...
     this.runIteration = this.runIteration.bind(this);
     this.handleLoadedManifest = this.handleLoadedManifest.bind(this);
   }
@@ -76,6 +77,11 @@ export default class NodeRunner {
     jwk: JWKInterface,
     nodeConfig: NodeConfig,
   ): Promise<NodeRunner> {
+    // Running a simple web server
+    // It should be called as early as possible
+    // Otherwise App Runner crashes ¯\_(ツ)_/¯
+    new ExpressAppRunner().run();
+
     const arweave = new ArweaveProxy(jwk);
     const providerAddress = await arweave.getAddress();
     const arweaveService = new ArweaveService(arweave, nodeConfig.minimumArBalance);
