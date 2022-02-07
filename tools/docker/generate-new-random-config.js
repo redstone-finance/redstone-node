c67d7c4b660853ec04131ef8d240f2384c099758
hatskier <suvorovalex1997@gmail.com>
2022-02-07 02:09:44 +0100

    created a script for automated random config generation

---
+++ tools/docker/generate-new-random-config.js
@@ -0,0 +1,55 @@
+const { ethers } = require("ethers");
+const Arweave = require("arweave/node");
+
+main();
+
+async function main() {
+  const { publicProviderDetails, config } = await generateNewRandomNodeConfig();
+
+  console.log(`\n\n== Public provider details ==\n`);
+  console.log(JSON.stringify(publicProviderDetails, null, 2));
+
+  console.log(`\n\n== Private provider config ==\n`);
+  console.log(JSON.stringify(config));
+}
+
+async function generateNewRandomNodeConfig() {
+  const arweave = Arweave.init({
+    host: "arweave.net", // Hostname or IP address for a Arweave host
+    port: 443,           // Port
+    protocol: "https",   // Network protocol http or https
+    timeout: 60000,      // Network request timeouts in milliseconds
+    logging: false,      // Enable network request logging
+  });
+
+  const evmWallet = ethers.Wallet.createRandom();
+  const jwk = await arweave.wallets.generate();
+  const arweaveAddress = await arweave.wallets.jwkToAddress(jwk);
+  const arweavePublicKey = getPublicKey(jwk);
+
+  return {
+    config: {
+      arweaveKeysJWK: jwk,
+      minimumArBalance: 0.1,
+      useManifestFromSmartContract: true,
+      addEvmSignature: true,
+      enableStreamrBroadcaster: true,
+      omitSourcesInArweaveTx: true,
+      credentials: {
+        ethereumPrivateKey: evmWallet.privateKey,
+      },
+    },
+    publicProviderDetails: {
+      evmAddress: evmWallet.address,
+      arweaveAddress,
+      arweavePublicKey,
+    },
+  };
+
+}
+
+// Public key is saved in the "n"
+// More info: https://docs.arweave.org/developers/server/http-api#addressing
+function getPublicKey(jwk) {
+  return jwk.n;
+}
