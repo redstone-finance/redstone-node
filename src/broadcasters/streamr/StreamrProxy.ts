e3f9f63f0062ce55a304a897d0bf9be8630ed8e4
cehali <cezary.haliniarz@gmail.com>
2022-07-14 15:41:28 +0200

    feat: update and adjust streamr integration

--- src/broadcasters/streamr/StreamrProxy.ts
+++ src/broadcasters/streamr/StreamrProxy.ts
@@ -1,4 +1,8 @@
-import { StreamrClient, StorageNode, StreamOperation } from "streamr-client";
+import {
+  StreamrClient,
+  StreamPermission,
+  STREAMR_STORAGE_NODE_GERMANY,
+} from "streamr-client";
 import { Consola } from "consola";
 
 const logger = require("../../utils/logger")("StreamrProxy") as Consola;
@@ -12,20 +16,14 @@ export class StreamrProxy {
     });
   }
 
-  // Returns stream id of the created (or previously created) stream
-  async tryCreateStream(name: string): Promise<string> {
-    const streamId = await this.getStreamIdForStreamName(name);
-    const streamExists = await this.doesStreamExist(streamId);
-    if (streamExists) {
-      logger.info(`Streamr stream already exists: ${streamId}`);
-      return streamId;
-    } else {
-      return await this.createStream(streamId);
-    }
-  }
-
   public async publishToStreamByName(data: any, streamName: string) {
     const streamId = await this.getStreamIdForStreamName(streamName);
+    const doesStreamExist = await this.doesStreamExist(streamId);
+    if (!doesStreamExist) {
+      throw new Error(
+        `Stream ${streamId} doesn't exist, please create Streamr stream before starting node`
+      );
+    }
     return await this.publish(data, streamId);
   }
 
@@ -38,37 +36,9 @@ export class StreamrProxy {
   }
 
   private async getStreamIdForStreamName(name: string): Promise<string> {
-    const publicAddress = await this.streamrClient.getUserId();
+    const publicAddress = await this.streamrClient.getAddress();
     const path = `/redstone-oracle/${name}`;
     return `${publicAddress}${path}`;
-  }
-
-  // This method creates and configures a stream.
-  // It enables historical data storage in STREAMR_GERMANY
-  // And allows everyone to get the stream and access the stream data
-  private async createStream(id: string): Promise<string> {
-    const stream = await this.streamrClient.createStream({
-      id,
-      storageDays: 7,
-      requireEncryptedData: false,
-      requireSignedData: false,
-      inactivityThresholdHours: 24 * 20, // 20 days
-    });
-
-    logger.info(`Stream created: ${stream.id}`);
-    await stream.addToStorageNode(StorageNode.STREAMR_GERMANY);
-    logger.info("Stream added to the storage node: STREAMR_GERMANY");
-    await stream.grantPermission(
-      StreamOperation.STREAM_SUBSCRIBE,
-      undefined /* anyone */
-    );
-    await stream.grantPermission(
-      StreamOperation.STREAM_GET,
-      undefined /* anyone */
-    );
-    logger.info(`Added permissions to the stream: ${stream.id}`);
-
-    return stream.id;
   }
 
   private async doesStreamExist(streamId: string): Promise<boolean> {
