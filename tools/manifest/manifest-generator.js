6bc65760f69b9bdafa933d62c1dadc50ebc7321e
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2022-08-16 13:59:02 +0200

    feat: add Evmos token (#133)

--- tools/manifest/manifest-generator.js
+++ tools/manifest/manifest-generator.js
@@ -36,6 +36,14 @@ function generateManifest({
     sourceTimeout,
     maxPriceDeviationPercent,
     evmChainId: 1,
+    httpBroadcasterURLs: [
+      "https://api.redstone.finance",
+      "https://vwx3eni8c7.eu-west-1.awsapprunner.com",
+      "https://container-service-1.dv9sai71f4rsq.eu-central-1.cs.amazonlightsail.com",
+    ],
+    enableStreamrBroadcaster: false,
+    disableSinglePricesBroadcastingInStreamr: true,
+    enableArweaveBackup: false,
     tokens: getTokensConfig(tokens),
   };
 }
